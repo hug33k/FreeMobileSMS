8251f331778ed71068adf05e1a8eae6b1f4dd845
Hugo SCHOCH <hugo.schoch@epitech.eu>
2015-09-14 22:04:18 +0200

    Push project

---
+++ README.md
@@ -0,0 +1,38 @@
+#Free Mobile SMS
+
+##Description
+
+Python client to send SMS via french mobile operator Free Mobile.
+
+##Configuration
+
+You have to write your informations in config.json like this :
+````json
+{
+    "login": YOUR_FREE_MOBILE_LOGIN,
+    "token": TOKEN_GIVEN_BY_FREE_MOBILE
+}
+````
+
+You can use a different name for config file but you have to launch script with specific flag (--config=FILE).
+
+##Usage
+
+````shell
+$>./sms.py "Hello World"
+200 Message send
+
+$>./sms.py --config=my_config.json "Hello world"
+200 Message send
+````
+
+##Possible return values
+
+* 200 : Message send
+* 400 : Missing parameter
+* 402 : Too much messages send
+    You have to wait a little to reuse it.
+* 403 : Service not enable
+    You can enable the service in your [Free Mobile panel](https://mobile.free.fr/moncompte/)
+* 500 : Server not available
+    You have to try later.
---
+++ sms.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python2.7
+
+import	json
+import	sys
+import	urllib
+
+class	FreeMobileSMS():
+
+	def	__init__(self, config="config.json"):
+		self.baseURL = "https://smsapi.free-mobile.fr"
+		self.route = "/sendmsg"
+		self.file = config
+		self.codes = [200, 400, 402, 403, 500]
+		self.messages = ["Message send", "Missing parameter", "Too much messages send", "Service not enable", "Server not available"]
+		try:
+			configFile = open(self.file)
+			dataFile = json.load(configFile)
+			self.login = dataFile["login"]
+			self.token = dataFile["token"]
+		except:
+			raise Exception("Error with file " + self.file + "\n")
+
+	def	makeRoute(self, message):
+		route = self.baseURL + self.route
+		route += "?user=" + self.login
+		route += "&pass=" + self.token
+		route += "&msg=" + urllib.quote(message)
+		return route
+
+	def	checkCode(self, code):
+		index = self.codes.index(code)
+		if index >= 0:
+			return self.messages[index]
+		else:
+			return "Error message not found"
+
+	def	send(self, message):
+		route = self.makeRoute(message)
+		res = urllib.urlopen(route)
+		code = int(res.getcode())
+		message = self.checkCode(code)
+		return code, message
+
+def	usage():
+	print("Usage :")
+	print("./sms.py [--config=file] message")
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		config = "config.json"
+		for arg in sys.argv:
+			if arg.startswith("--config="):
+				tab = arg.split("=")
+				if (len(tab) > 1 and len(tab[1])):
+					config = tab[1]
+					sys.argv.remove(arg)
+				else:
+					usage()
+					sys.exit(1)
+		sms = FreeMobileSMS(config)
+		code, message = sms.send(sys.argv[1])
+		print (str(code) + " " + message)
+	else:
+		usage()
+		sys.exit(1)
